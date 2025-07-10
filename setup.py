0366a950a4ac225d855fe2b23445fe2418f5f381
bishal-dev-omnidim <bishal@omnidim.io>
2025-07-10 20:00:06 +0530

    build: bump version to 0.2.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omnidimension",
-    version="0.2.10",
+    version="0.2.11",
     packages=find_packages() + ["omnidim_mcp_server"],
     install_requires=["requests"],
     extras_require={
